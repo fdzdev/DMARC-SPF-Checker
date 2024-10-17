4b944753f133b0586548fff90b6265f5c3a1b223
Facundo Fernandez <facu.tha@gmail.com>
2024-10-17 09:33:40 -0600

    Fixed issues

--- mailwatch.py
+++ mailwatch.py
@@ -128,7 +128,7 @@ def main():
 
         # Handle DMARC issues
         bold_domain = f"\033[1m{domain}\033[0m"
-        if dmarc_policy == "none" or dmarc_policy == "No DMARC Policy found":
+        if dmarc_policy is None or dmarc_policy == "No DMARC Policy found":
             cprint(
                 f"DMARC issue found for {bold_domain}! DMARC Policy not being enforced",
                 "red",
