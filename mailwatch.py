0cba12876c797dd13087a8ffa8d1b2f9c93be59e
Facundo Fernandez <facu.tha@gmail.com>
2024-11-11 15:32:55 -0700

    Update

--- mailwatch.py
+++ mailwatch.py
@@ -152,9 +152,9 @@ def main():
                 )
 
             if user_input == "1":
-                spoofed_sender_email = f"RTX@{domain}"
+                spoofed_sender_email = f"ceo@{domain}"
                 recipient_email = (
-                    "gomezpaz.mat@gmail.com"  # CHANGE YOUR EMAIL ADDRESS HERE!!!!
+                    "facu.tha@gmail.com"  # CHANGE YOUR EMAIL ADDRESS HERE!!!!
                 )
                 send_spoofed_email(spoofed_sender_email, recipient_email)
             elif user_input == "2":
